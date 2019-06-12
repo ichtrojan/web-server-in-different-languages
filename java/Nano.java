9a0dd18258d12e25f09e142dccf3edb103ba3658
Olatunji <Olatunji4you@gmail.com>
2019-06-11 18:18:05 -0700

    Minimalist Nano server in Java

---
+++ java/Nano.java
@@ -0,0 +1,20 @@
+package com.olatunji.nano;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Tunji035
+ */
+public class Nano {
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(9080);
+        try (Socket socket = ss.accept()) {
+            socket.getOutputStream().write("Nano server running! powered by Java!".getBytes());
+        }
+    }
+    
+}
---
+++ java/README.md
@@ -0,0 +1,11 @@
+# Minimalistic Nano server in Java :-)
+
+**Note**
+
+> You must have [Jdk](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html) installed on your local machine
+
+Run the command below:
+
+```shell
+curl http://localhost:8080
+```
